ab31241793c106dd9e5561a477ab18ac2ff5b75b
Kiran Patel <kiran@inteleyes.com>
2023-02-14 09:30:38 +0000

    Restructured and cleaned up index.ts

--- src/Server.ts
+++ src/Server.ts
@@ -1,35 +1,11 @@
-import * as express from 'express';
+// import * as express from 'express';
+// import main from "./index";
 
-class Server {
-	public express
+// const router = express.Router();
 
-	constructor() {
-		this.express = express();
-		this.loadRoutes()
-	}
 
-	private loadRoutes() : void {
-
-		
-		
-		// this.express.get('/', (req, res) => {
-		//     res.json({
-			//         'message': 'Hello World!'
-		//     })
-		// })
-		this.express.use(express.json( { limit: '2mb' }));
-
-		// this.express.use("/", (req, res, err) => { 
-		//     console.log(req.body);
-		// })
-
-		this.express.post('/NotificationInfo/TollgateInfo', (req: express.Request, res: express.Response) => { 
-			console.log(req); 
-		})
-	} 
-}
-
-export default new Server().express;
+// export default router;
+// export default new Server().express;
 
 
 // https://medium.com/@pmhegdek/oop-in-typescript-express-server-d9368b97740e
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
 
+import * as express from "express";
+// import router from "./Server";
 
-const { Client } = require('pg');
-import * as pg from "pg";
-import server from "./Server";
+const { Client } = require("pg");
 require("dotenv").config();
 
-const serverListenPort = 8000;
-
-// https://northflank.com/guides/connecting-to-a-postgresql-database-using-node-js
-// https://medium.com/bb-tutorials-and-thoughts/how-to-build-nodejs-rest-api-with-express-and-postgresql-typescript-version-121b5a11c9a6
-
-// https://www.digitalocean.com/community/tutorials/setting-up-a-node-project-with-typescript
 
 // connect to local Postgresql database using credentials in the .env file
 const getClient = async () => {
@@ -26,6 +20,87 @@ const getClient = async () => {
   return client;
 };
 
+
+class Main {
+	
+	private lastNumberplate:string = "";
+	private dbClient: typeof Client;
+
+	private serverListenPort = 8000;
+
+	public server: express.Application;
+	
+	constructor() {
+		this.server = express();
+		this.loadRoutes();
+		this.server.listen(this.serverListenPort, () => { console.log("server listening") });
+		
+		this.dbClient = async () => {
+			return await getClient();
+		}
+		
+		// this.makeDBQuery("SELECT * FROM \"Log\"").then((result:pg.QueryResult) => {
+		//   console.log(result.rows);
+		// })
+
+	}
+	
+	public handleNumberplateEvent(request:express.Request):void {
+		console.log(request.body["Picture"].Plate.PlateNumber)
+	}
+
+	private async makeDBQuery(query:string):Promise<any> {
+		const dataPromise = this.dbClient.query(query);
+		this.dbClient.end()
+		return dataPromise;
+	}
+	
+	private loadRoutes(): void {
+		
+		this.server.use(express.json( { limit: "2mb" } ));
+
+		// this.express.use("/", (req, res) => {
+		// 	console.log(req.body);
+		// })
+
+		this.server.post("/NotificationInfo/TollgateInfo", (req: express.Request, res: express.Response) => { 
+			console.log(req);
+			this.handleNumberplateEvent(req);
+		})
+
+		this.server.post("/insert/:table/", () => {
+			
+		})
+
+		this.server.post("/update/:table/", () => {
+
+		})
+	}
+	
+}
+
+
+
+export default new Main().server;
+
+// const server = express();
+
+// server.use(express.json( { limit: "2mb" }));
+
+// server.post("/NotificationInfo/TollgateInfo", (req: Request, res: Response) => { 
+	// 	console.log(req);
+	// })
+	
+	// server.listen(serverListenPort, () => {
+		// 	// if (err) return console.log(err);
+		// 	console.log("Server running on port ", serverListenPort);
+	
+// })
+
+// https://northflank.com/guides/connecting-to-a-postgresql-database-using-node-js
+// https://medium.com/bb-tutorials-and-thoughts/how-to-build-nodejs-rest-api-with-express-and-postgresql-typescript-version-121b5a11c9a6
+// https://www.digitalocean.com/community/tutorials/setting-up-a-node-project-with-typescript
+
 // sample query
 // (async () => {
 // 	const dbClient = await getClient();
@@ -33,38 +108,3 @@ const getClient = async () => {
 // 	console.log(log_data.rows);
 // 	await dbClient.end();
 // })
-
-class Main {
-
-	private lastNumberplate:string = "";
-	private dbClient: typeof Client;
-
-	constructor() {
-		this.dbClient = async () => {
-			return await getClient();
-		}
-
-		this.makeDBQuery("SELECT * FROM \"Log\"").then((result:pg.QueryResult) => {
-		  console.log(result.rows);
-		})
-	}
-
-	public handleNumberplateEvent():void {
-
-	}
-
-	private async makeDBQuery(query:string):Promise<any> {
-	  const dataPromise = this.dbClient.query(query);
-	  this.dbClient.end()
-	  return dataPromise;
-	}
-
-}
-
-
-
-server.listen(serverListenPort, () => {
-	// if (err) return console.log(err);
-	console.log("Server running on port ", serverListenPort);
-	
-})
