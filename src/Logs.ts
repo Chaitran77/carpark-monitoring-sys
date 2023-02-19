a6a93868df9526b82f9d049e0bc46124ccd8fb67
Kiran Patel <kiran@inteleyes.com>
2023-02-19 20:49:47 +0000

    Eliminated memory leak!
    This was causing the process to be killed after approx. 15 minutes, since each Log() contains an approx. 0.6MB base64-encoded image, and 5 Log() objects were being added every 10s.

--- src/Logs.ts
+++ src/Logs.ts
@@ -13,6 +13,8 @@ abstract class Logs {
     public static async loadLogs(count:number) { // loads the most recently created logs into Logs.logs[]
         const logRecords = (await await dbQuery.makeDBQuery(`SELECT log_id, camera_id, numberplate, entry_timestamp, exit_timestamp, vehicle_id, entry_image_base64, exit_image_base64, acknowleged, known_vehicle FROM "Log" ORDER BY log_id LIMIT $1;`, [count.toString()])).rows;
 
+        // clear Logs.logs[], then populate with new Log() instances
+        Logs.logs = [];
         for (let i = 0; i < logRecords.length; i++) {
             const rec = logRecords[i];
             this.addLog(new Log(rec.log_id, rec.camera_id, rec.vehicle_id, rec.numberplate, rec.entry_timestamp, rec.exit_timestamp, rec.entry_image_base64, rec.exit_image_base64, rec.acknowleged, rec.known_vehicle));
